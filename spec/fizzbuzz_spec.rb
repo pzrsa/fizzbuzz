ebe3d688214fd182efc4c51e1c4b1553362cdb6a
parsa <parsamesg@gmail.com>
2021-09-16 15:00:05 +0100

    add testcases for fizzbuzz

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,13 @@
 require "fizzbuzz"
 
 describe "fizzbuzz" do
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq "fizz"
+  it 'returns "fizz" when passed a multiple of 3' do
+    expect(fizzbuzz(rand(100) * 3)).to eq "fizz"
+  end
+  it 'return "buzz" when passed a multiple of 5' do
+    expect(fizzbuzz(rand(100) * 5)).to eq "buzz"
+  end
+  it 'return "fizzbuzz" when passed a multiple of 3 and 5' do
+    expect(fizzbuzz(rand(100) * 15)).to eq "fizzbuzz"
   end
 end
