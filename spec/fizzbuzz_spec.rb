9a7ab63fe212d2d2dc161be14a47b98b7be6bbe1
Anmol <bhatianmol566@yahoo.co.uk>
2021-09-16 17:02:15 +0100

    Add test case for number that isn't multiple of 3 or 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,12 +2,15 @@ require "fizzbuzz"
 
 describe "fizzbuzz" do
   it 'returns "fizz" when passed a multiple of 3' do
-    expect(fizzbuzz(rand(100) * 3)).to eq "fizz"
+    expect(fizzbuzz(3)).to eq "fizz"
   end
   it 'return "buzz" when passed a multiple of 5' do
-    expect(fizzbuzz(rand(100) * 5)).to eq "buzz"
+    expect(fizzbuzz(5)).to eq "buzz"
   end
   it 'return "fizzbuzz" when passed a multiple of 3 and 5' do
-    expect(fizzbuzz(rand(100) * 15)).to eq "fizzbuzz"
+    expect(fizzbuzz(15)).to eq "fizzbuzz"
+  end
+  it 'return number itself passed if neither a multiple of 3 nor 5' do
+    expect(fizzbuzz(7)).to eq 7
   end
 end
