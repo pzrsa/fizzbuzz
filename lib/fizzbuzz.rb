af01963a4b96e05c6d7224fe63193efac446118d
parsa <parsamesg@gmail.com>
2021-09-17 12:52:16 +0100

    capitalise fizz, buzz and fizzbuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,10 +1,10 @@
-def fizzbuzz(num)
+def FizzBuzz(num)
   if num % 15 == 0
-    return "fizzbuzz"
+    return "FizzBuzz"
   elsif num % 3 == 0
-    return "fizz"
+    return "Fizz"
   elsif num % 5 == 0
-    return "buzz"
+    return "Buzz"
   else
     return num
   end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,44 +1,44 @@
-require "fizzbuzz"
+require "FizzBuzz"
 
-describe "fizzbuzz" do
-  it 'returns "fizz" when passed a multiple of 3' do
-    expect(fizzbuzz(3)).to eq "fizz"
+describe "FizzBuzz" do
+  it 'returns "Fizz" when passed a multiple of 3' do
+    expect(FizzBuzz(3)).to eq "Fizz"
   end
-  it 'return "buzz" when passed a multiple of 5' do
-    expect(fizzbuzz(5)).to eq "buzz"
+  it 'return "Buzz" when passed a multiple of 5' do
+    expect(FizzBuzz(5)).to eq "Buzz"
   end
-  it 'return "fizzbuzz" when passed a multiple of 3 and 5' do
-    expect(fizzbuzz(15)).to eq "fizzbuzz"
+  it 'return "FizzBuzz" when passed a multiple of 3 and 5' do
+    expect(FizzBuzz(15)).to eq "FizzBuzz"
   end
-  it 'return number itself passed if neither a multiple of 3 nor 5' do
-    expect(fizzbuzz(7)).to eq 7
+  it "return number itself passed if neither a multiple of 3 nor 5" do
+    expect(FizzBuzz(7)).to eq 7
   end
 
-  it 'return "fizz" when passed a random multiple of 3' do
+  it 'return "Fizz" when passed a random multiple of 3' do
     num = 0
     until num.remainder(3).zero? && !num.remainder(5).zero?
       num = rand(1..1000) * 3
     end
-    expect(fizzbuzz(num)).to eq "fizz"
+    expect(FizzBuzz(num)).to eq "Fizz"
   end
 
-  it 'return "buzz" when passed a random multiple of 5' do
+  it 'return "Buzz" when passed a random multiple of 5' do
     num = 0
     until num.remainder(5).zero? && !num.remainder(3).zero?
       num = rand(1..1000) * 5
     end
-    expect(fizzbuzz(num)).to eq "buzz"
+    expect(FizzBuzz(num)).to eq "Buzz"
   end
 
-  it 'return "fizzbuzz" when passed a random multiple of 3 and 5' do
-    expect(fizzbuzz(rand(1000) * 15)).to eq "fizzbuzz"
+  it 'return "FizzBuzz" when passed a random multiple of 3 and 5' do
+    expect(FizzBuzz(rand(1000) * 15)).to eq "FizzBuzz"
   end
 
-  it 'return number itself when passed a random number that is neither a multiple of 3 nor 5' do
+  it "return number itself when passed a random number that is neither a multiple of 3 nor 5" do
     num = 0
     while num.remainder(3).zero? || num.remainder(5).zero?
       num = rand(1..1000)
     end
-    expect(fizzbuzz(num)).to eq num
+    expect(FizzBuzz(num)).to eq num
   end
 end
